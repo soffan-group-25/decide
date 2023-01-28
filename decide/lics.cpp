04144ec0c24077a3abc4b1f6fa543baa57a60000
Didrik Munther <dmu0817@gmail.com>
2023-01-28 07:40:34 +0100

    Add lic0 main loop

--- decide/lics.cpp
+++ decide/lics.cpp
@@ -1,48 +1,60 @@
+#include <decide/decide.hpp>
 #include <decide/lics.hpp>
 #include <iostream>
 
-bool lic0(){
+bool lic0() {
+    double length = parameters.LENGTH1;
+    Coordinate* last = &points[0];
+    for (int i = 1; i < points.size(); i++) {
+        Coordinate* current = &points[i];
+        double distance = 0; // todo: check distance between 2 coordinates
+        if(distance > length) { 
+            return true;
+        }
+    }
+
     return false;
 }
-bool lic1(){
+
+bool lic1() {
     return false;
 }
-bool lic2(){
+bool lic2() {
     return false;
 }
-bool lic3(){
+bool lic3() {
     return false;
 }
-bool lic4(){
+bool lic4() {
     return false;
 }
-bool lic5(){
+bool lic5() {
     return false;
 }
-bool lic6(){
+bool lic6() {
     return false;
 }
-bool lic7(){
+bool lic7() {
     return false;
 }
-bool lic8(){
+bool lic8() {
     return false;
 }
-bool lic9(){
+bool lic9() {
     return false;
 }
-bool lic10(){
+bool lic10() {
     return false;
 }
-bool lic11(){
+bool lic11() {
     return false;
 }
-bool lic12(){
+bool lic12() {
     return false;
 }
-bool lic13(){
+bool lic13() {
     return false;
 }
-bool lic14(){
+bool lic14() {
     return false;
 }
