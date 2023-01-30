6e4864c63f231e446e680423e21bd9a0a0b98b08
Didrik Munther <dmu0817@gmail.com>
2023-01-30 23:21:50 +0100

    Add distance from point test for lic6

--- decide/lic6.test.cpp
+++ decide/lic6.test.cpp
@@ -16,6 +16,17 @@ void test_distance_from_line() {
   assert(abs(result - 9.0 / 5.0) < 0.0001);
 }
 
+// The point is closest to another point,
+// namely sqrt(2) away
+void test_distance_from_point() {
+  auto point = Coordinate(2, 2);
+  auto start = Coordinate(1, 1);
+  auto end = Coordinate(0, 0);
+  double result = distanceFromLine(point, start, end);
+  assert(abs(result - sqrt(2.0)) < 0.0001);
+}
+
 int main() {
   test_distance_from_line();
+  test_distance_from_point();
 }
