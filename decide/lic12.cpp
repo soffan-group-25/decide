a1ae1631df128e0e3eeb64c5b779271f35ea7111
Didrik Munther <dmu0817@gmail.com>
2023-01-30 12:58:52 +0100

    Add lic12 skeleton

---
+++ decide/lic12.cpp
@@ -0,0 +1,5 @@
+#include <decide/decide.hpp>
+
+bool lic12(Points points, Parameters parameters) {
+    return false; // todo: implement
+}
--- decide/lics.hpp
+++ decide/lics.hpp
@@ -5,5 +5,6 @@
 
 // Functions
 bool lic0(Points points, Parameters parameters);
+bool lic12(Points points, Parameters parameters);
 
 #endif
