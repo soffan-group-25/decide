d54252de149486c02ad87f3cd8342b0ab35b932f
haval0 <56519858+haval0@users.noreply.github.com>
2023-01-30 15:07:59 +0100

    Add (unfinished) tests for lic5

---
+++ decide/lic5.test.cpp
@@ -0,0 +1,21 @@
+#include <cassert>
+#include <cstdlib>
+#include <decide/decide.hpp>
+#include <decide/lics.hpp>
+
+void test_true_instance() {
+  Points points{Coordinate(1, 0), Coordinate(0, 0)};
+  Parameters parameters{};
+  assert(lic5(points, parameters));
+}
+
+void test_false_instance() {
+  Points points{Coordinate(0, 0), Coordinate(0, 0)};
+  Parameters parameters{};
+  assert(lic5(points, parameters));
+}
+
+int main() {
+  test_true_instance();
+  test_false_instance();
+}
