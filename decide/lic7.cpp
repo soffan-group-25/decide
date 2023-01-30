018e4337bb7c9e77c3c8d7cb4742b4cd1efbd361
Didrik Munther <dmu0817@gmail.com>
2023-01-30 14:35:08 +0100

    Remove assertions from lic7 function

--- decide/lic7.cpp
+++ decide/lic7.cpp
@@ -1,14 +1,11 @@
 #include <cassert>
 #include <decide/decide.hpp>
 
+// 1 <= K_PTS <= (NUMPOINTS - 2). According to specification.
 bool lic7(Points points, Parameters parameters) {
   int npts = points.size();
   int kpts = parameters.KPTS;
   double length = parameters.LENGTH1;
-
-  // 1 <= K_PTS <= (NUMPOINTS - 2). According to specification.
-  assert(1 <= kpts);
-  assert(kpts <= npts - 2);
 
   // The condition is not met when NUMPOINTS < 3
   if (npts < 3)
