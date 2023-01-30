c98d8ff85b67d173d4585aaf63b8595911e47f6d
Daniel Williams <dwilli@kth.se>
2023-01-30 02:12:13 +0100

    Add cstdlib include for std::exit

--- decide/lic0.test.cpp
+++ decide/lic0.test.cpp
@@ -1,10 +1,11 @@
+#include <cstdlib>
 #include <decide/decide.hpp>
 #include <decide/lics.hpp>
 
 int main() {
-    Points points{Coordinate(1, 1)};
-    Parameters parameters{
-        .LENGTH1 = 1};
+  Points points{Coordinate(1, 1)};
+  Parameters parameters{.LENGTH1 = 1};
 
-    if (lic0(points, parameters)) std::exit(EXIT_FAILURE);
+  if (lic0(points, parameters))
+    std::exit(EXIT_FAILURE);
 }
