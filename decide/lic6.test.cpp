51c36a5088ca2a8dc7dc0e28fff5b3ea40540d6e
Didrik Munther <dmu0817@gmail.com>
2023-01-30 23:26:20 +0100

    Add tests for LIC6

--- decide/lic6.test.cpp
+++ decide/lic6.test.cpp
@@ -26,7 +26,28 @@ void test_distance_from_point() {
   assert(abs(result - sqrt(2.0)) < 0.0001);
 }
 
+// The coordinate (5,11) will be >10 above the line
+// created by (0,0), (10, 0).
+void test_true_instance() {
+  Points points{Coordinate(-1, 0), Coordinate(0, 0), Coordinate(5, 11),
+                Coordinate(10, 0), Coordinate(11, 0)};
+  Parameters parameters{.NPTS = 1, .DIST = 10.0};
+  assert(lic6(points, parameters));
+}
+
+// The largest possible distance from a point to a line,
+// namely (5,9) to the line (0,0) -> (10, 0) will not be sufficiently
+// large for the value of DIST=10.0.
+void test_false_instance() {
+  Points points{Coordinate(-1, 0), Coordinate(0, 0), Coordinate(5, 9),
+                Coordinate(10, 0), Coordinate(11, 0)};
+  Parameters parameters{.NPTS = 1, .DIST = 10.0};
+  assert(!lic6(points, parameters));
+}
+
 int main() {
   test_distance_from_line();
   test_distance_from_point();
+  test_true_instance();
+  test_false_instance();
 }
