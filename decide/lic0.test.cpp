64a3e5258d53024ea13b23d483d16d1c050aa570
Didrik Munther <dmu0817@gmail.com>
2023-01-30 10:51:22 +0100

    Add tests for lic0

--- decide/lic0.test.cpp
+++ decide/lic0.test.cpp
@@ -2,10 +2,19 @@
 #include <decide/decide.hpp>
 #include <decide/lics.hpp>
 
-int main() {
-  Points points{Coordinate(1, 1)};
-  Parameters parameters{.LENGTH1 = 1};
+void test_true_instance() {
+    Points points{Coordinate(1, 1), Coordinate(2, 2)};
+    Parameters parameters{.LENGTH1 = 1};
+    assert(lic0(points, parameters));
+}
 
-  if (lic0(points, parameters))
-    std::exit(EXIT_FAILURE);
+void test_false_instance() {
+    Points points{Coordinate(1, 1), Coordinate(2, 2)};
+    Parameters parameters{.LENGTH1 = 3};
+    assert(!lic0(points, parameters));
+}
+
+int main() {
+    test_true_instance();
+    test_false_instance();
 }
