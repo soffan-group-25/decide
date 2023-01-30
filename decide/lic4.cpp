d97802fda818a6d04abfa51a7e0ea7b618505232
hebbs4152 <hannahburak@gmail.com>
2023-01-30 14:46:43 +0100

    first attempt att lic4

--- decide/lic4.cpp
+++ decide/lic4.cpp
@@ -1,5 +1,90 @@
 #include <decide/decide.hpp>
 
-bool lic12(Points points, Parameters parameters) {
-    return false; // todo: implement
+/*There exists at least one set of Q_PTS (consecutive data points) that lie in more than QUADS
+(quadrants). Where there is ambiguity as to which quadrant contains a given point, priority
+of decision will be by quadrant number, i.e., I, II, III, IV. For example, the data point (0,0)
+is in quadrant I, the point (-l,0) is in quadrant II, the point (0,-l) is in quadrant III, the point
+(0,1) is in quadrant I and the point (1,0) is in quadrant I.*/
+bool lic4(Points points, int qpts, int quads){ 
+    int npts=points.size();
+    if (qpts<2 || qpts>npts){
+        return false;
+    }
+    switch (quads)
+    {
+    case 1:
+        return true;
+
+    case 2:
+        for (int i=0; i<npts;i++){
+            int position[qpts];
+            for (int a=0; a<qpts; a++){
+                position[a]=quad(points[i]);
+            }
+            if (unique(position, 3)){
+                return true;
+            }
+        }
+        return false;
+    case 3:
+        for (int i=0; i<npts;i++){
+            int position[qpts];
+            for (int a=0; a<qpts; a++){
+                position[a]=quad(points[i+a]);
+            }
+            if (unique(position, 3)){
+                return true;
+            }
+        }
+        return false;
+    default:
+        return false;
+    }
+
+    for (int i=0; i < npts; i++){
+        for (int  )
+        int q= quad(points[i]);
+
+
+    }
+
+
+}
+
+int quad(Coordinate point){
+    if (point.x>=0){
+        if (point.y>=0){
+            return 1;
+        }
+        else{
+            return 4;
+        }
+    }
+    else{
+        if (point.y>=0){
+            return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+}
+
+bool unique(int arr[], int goal){
+    int uni=1;
+    
+    for (int i=1; i<sizeof(arr); i++){
+        int a = 0;
+        for (a=0; a < i;a++) 
+            if (arr[i]==arr[a]){
+                break;
+            }
+        if (i==a){
+            uni++;
+        }
+        if (uni == goal){
+            return true;
+        }
+    }
+    return false;
 }
