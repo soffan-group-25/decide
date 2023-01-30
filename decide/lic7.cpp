f0abbb55a41dfdea193f0cca889c4f30150e5cb3
Didrik Munther <dmu0817@gmail.com>
2023-01-30 11:39:41 +0100

    Add skeleton for LIC7

---
+++ decide/lic7.cpp
@@ -0,0 +1,5 @@
+#include <decide/decide.hpp>
+
+bool lic7(Points points, Parameters parameters) {
+    return false; // todo: implement
+}
--- decide/lics.hpp
+++ decide/lics.hpp
@@ -5,6 +5,7 @@
 
 // Functions
 bool lic0(Points points, Parameters parameters);
+bool lic7(Points points, Parameters parameters);
 bool lic12(Points points, Parameters parameters);
 
 #endif
