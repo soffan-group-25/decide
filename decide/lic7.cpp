179dac0e277e076e9f87098aa66f5ab5bc9b2915
Didrik Munther <dmu0817@gmail.com>
2023-01-30 11:48:44 +0100

    Implement lic7 function

--- decide/lic7.cpp
+++ decide/lic7.cpp
@@ -1,5 +1,29 @@
+#include <cassert>
 #include <decide/decide.hpp>
 
 bool lic7(Points points, Parameters parameters) {
-    return false; // todo: implement
+  int npts = points.size();
+  int kpts = parameters.KPTS;
+  double length = parameters.LENGTH1;
+
+  // 1 <= K_PTS <= (NUMPOINTS - 2). According to specification.
+  assert(1 <= kpts);
+  assert(kpts <= npts - 2);
+
+  // The condition is not met when NUMPOINTS < 3
+  if (npts < 3)
+    return false;
+
+  Coordinate *last = &points[0];
+
+  // Next point separated by kpts points.
+  for (int i = 2 + kpts; i < npts; i++) {
+    Coordinate *current = &points[i];
+    double distance = current->distance(*last);
+
+    if (distance > length)
+      return true;
+  }
+
+  return false;
 }
