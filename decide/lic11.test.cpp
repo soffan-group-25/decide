ae02b2b6ea62b9f9631e7581d27068f4be34fb3e
Didrik Munther <dmu0817@gmail.com>
2023-01-30 16:56:50 +0100

    Add lic11 OBOE test

--- decide/lic11.test.cpp
+++ decide/lic11.test.cpp
@@ -4,17 +4,24 @@
 #include <decide/lics.hpp>
 
 // Should be fulfilled because points
-// (50,50) and (4,4) results in X[j]-X[i]=4-50<0.
+// (5,5) and (1,1) results in X[j]-X[i]=1-5<0.
 void test_true_instance() {
-  Points points{Coordinate(50, 50), Coordinate(2, 2), Coordinate(3, 3),
-                Coordinate(4, 4),   Coordinate(5, 5), Coordinate(100, 100)};
+  Points points{Coordinate(5, 5), Coordinate(5, 5), Coordinate(5, 5),
+                Coordinate(5, 5), Coordinate(1, 1), Coordinate(5, 5)};
   Parameters parameters{.GPTS = 2};
   assert(lic11(points, parameters));
 }
 
 // Should not be fulfilled because
 // there is no point in which X[j]-X[i]<0 for any j>i.
-void test_false_instance() {
+void test_false_instance1() {
+  Points points{Coordinate(5, 5), Coordinate(5, 5), Coordinate(1, 1),
+                Coordinate(5, 5), Coordinate(5, 5), Coordinate(5, 5)};
+  Parameters parameters{.GPTS = 2};
+  assert(!lic11(points, parameters));
+}
+
+void test_false_instance2() {
   Points points{Coordinate(1, 1), Coordinate(2, 2), Coordinate(3, 3),
                 Coordinate(4, 4), Coordinate(5, 5), Coordinate(100, 100)};
   Parameters parameters{.GPTS = 2};
@@ -23,5 +30,6 @@ void test_false_instance() {
 
 int main() {
   test_true_instance();
-  test_false_instance();
+  test_false_instance1();
+  test_false_instance2();
 }
