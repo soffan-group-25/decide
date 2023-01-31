27c0f367cda3563576cb66a96dc51e3fd585b0fd
Didrik Munther <dmu0817@gmail.com>
2023-01-31 02:26:31 +0100

    Format lic14

--- decide/lic14.cpp
+++ decide/lic14.cpp
@@ -1,53 +1,56 @@
 #include <decide/decide.hpp>
 
-/*There exists at least one set of three data points, separated by exactly E PTS and F PTS consecutive intervening points, respectively, that are the vertices of a triangle with area greater
-than AREA1. In addition, there exist three data points (which can be the same or different
-from the three data points just mentioned) separated by exactly E PTS and F PTS consecutive intervening points, respectively, that are the vertices of a triangle with area less than
-AREA2. Both parts must be true for the LIC to be true. The condition is not met when
+/*There exists at least one set of three data points, separated by exactly E PTS
+and F PTS consecutive intervening points, respectively, that are the vertices of
+a triangle with area greater than AREA1. In addition, there exist three data
+points (which can be the same or different from the three data points just
+mentioned) separated by exactly E PTS and F PTS consecutive intervening points,
+respectively, that are the vertices of a triangle with area less than AREA2.
+Both parts must be true for the LIC to be true. The condition is not met when
 NUMPOINTS < 5.
 0 ≤ AREA2*/
 
-bool lic14(Points points, Parameters param){
-    int npts=points.size();
-    int area1= param.AREA1;
-    int area2=param.AREA2;
-    int e=param.EPTS+1;
-    int f=param.FPTS+1;
-    
-    if (npts<5 || area2< 0){
-        return false;
-    }
+bool lic14(Points points, Parameters param) {
+  int npts = points.size();
+  int area1 = param.AREA1;
+  int area2 = param.AREA2;
+  int e = param.EPTS + 1;
+  int f = param.FPTS + 1;
 
-    bool first;
-    bool second;
-    int area;
-
-    for (int i=0; i<npts; i++){
-        if (i+e+f>npts){
-            return false;
-        }
-        Coordinate p1=points[i];
-        Coordinate p2=points[i+e];
-        Coordinate p3=points[i+e+f];
-        area=(p1.x*(p2.y-p3.y)+p2.x*(p3.y-p1.y)+p3.x*(p1.y-p2.y))/2;
-        if (area<0){
-            area=area*-1;
-        }
-
-        if (!first){
-            if (area>area1){
-                first= true;
-            }
-
-        }
-        if (!second){
-            if (area<area2){
-            second= true;
-            }       
-        }
-        else if(first && second){
-            return true;
-        }
-    }
+  if (npts < 5 || area2 < 0) {
     return false;
+  }
+
+  bool first;
+  bool second;
+  int area;
+
+  for (int i = 0; i < npts; i++) {
+    if (i + e + f > npts) {
+      return false;
+    }
+    Coordinate p1 = points[i];
+    Coordinate p2 = points[i + e];
+    Coordinate p3 = points[i + e + f];
+    area =
+        (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) /
+        2;
+    if (area < 0) {
+      area = area * -1;
+    }
+
+    if (!first) {
+      if (area > area1) {
+        first = true;
+      }
+    }
+    if (!second) {
+      if (area < area2) {
+        second = true;
+      }
+    } else if (first && second) {
+      return true;
+    }
+  }
+  return false;
 }
--- decide/lic14.tests.cpp
+++ decide/lic14.tests.cpp
@@ -3,117 +3,121 @@
 #include <decide/decide.hpp>
 #include <decide/lics.hpp>
 
-//both areas are wrong 
+// both areas are wrong
 void test_false_instance1() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0)};
-    Parameters parameters{.AREA1 = 500, .EPTS=2, .FPTS=3, .AREA2= 0 };
-    assert(lic14(points, parameters));
+  Points points{
+      Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),  Coordinate(3, 4),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0),  Coordinate(5, -6),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0),  Coordinate(1, -2),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(3, 4),  Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(5, -6), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0)};
+  Parameters parameters{.AREA1 = 500, .EPTS = 2, .FPTS = 3, .AREA2 = 0};
+  assert(lic14(points, parameters));
 }
 
-
-//first area is wrong
+// first area is wrong
 void test_false_instance2() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), 
-                     Coordinate(5, -6), Coordinate(0, 0), 
-                     Coordinate(1, -2), Coordinate(0, 0), 
-                     Coordinate(3, 4), Coordinate(0, 0), 
-                     Coordinate(5, -6), Coordinate(0, 0)};
-    Parameters parameters{.AREA1 = 500, .EPTS=1, .FPTS=1, .AREA2= 500 };
-    assert(lic14(points, parameters));
+  Points points{Coordinate(1, -2), Coordinate(0, 0),  Coordinate(3, 4),
+                Coordinate(0, 0),  Coordinate(5, -6), Coordinate(0, 0),
+                Coordinate(1, -2), Coordinate(0, 0),  Coordinate(3, 4),
+                Coordinate(0, 0),  Coordinate(5, -6), Coordinate(0, 0)};
+  Parameters parameters{.AREA1 = 500, .EPTS = 1, .FPTS = 1, .AREA2 = 500};
+  assert(lic14(points, parameters));
 }
 
-
-//second area is wrong
+// second area is wrong
 void test_false_instance3() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(5, -6)};
-    Parameters parameters{.AREA1 = 0, .EPTS=3, .FPTS=2, .AREA2= 0 };
-    assert(lic14(points, parameters));
+  Points points{
+      Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(3, 4),  Coordinate(0, 0), Coordinate(0, 0), Coordinate(5, -6),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0), Coordinate(1, -2),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0), Coordinate(3, 4),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(5, -6)};
+  Parameters parameters{.AREA1 = 0, .EPTS = 3, .FPTS = 2, .AREA2 = 0};
+  assert(lic14(points, parameters));
 }
 
-//not enough points 
-//second check
+// not enough points
+// second check
 void test_false_instance4() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0)};
-    Parameters parameters{.AREA1 = 0, .EPTS=7, .FPTS=18, .AREA2=500  };
-    assert(lic14(points, parameters));
+  Points points{
+      Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),  Coordinate(3, 4),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0),  Coordinate(5, -6),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0),  Coordinate(1, -2),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(3, 4),  Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(5, -6), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0)};
+  Parameters parameters{.AREA1 = 0, .EPTS = 7, .FPTS = 18, .AREA2 = 500};
+  assert(lic14(points, parameters));
 }
 
-//not enough points
-//first check
+// not enough points
+// first check
 void test_false_instance5() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0)};
-    Parameters parameters{.AREA1 = 2, .EPTS=2, .FPTS=2, .AREA2= 0 };
-    assert(lic14(points, parameters));
+  Points points{Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0)};
+  Parameters parameters{.AREA1 = 2, .EPTS = 2, .FPTS = 2, .AREA2 = 0};
+  assert(lic14(points, parameters));
 }
 
-//the triangles are different
-//smaller triangle shows up first
+// the triangles are different
+// smaller triangle shows up first
 void test_true_instance1() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(1, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),Coordinate(0, 0), Coordinate(0, 0),Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0)};
-    Parameters parameters{.AREA1 = 12, .EPTS=4, .FPTS=6, .AREA2= 6 };
-    assert(lic14(points, parameters));
+  Points points{
+      Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(1, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
+      Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0)};
+  Parameters parameters{.AREA1 = 12, .EPTS = 4, .FPTS = 6, .AREA2 = 6};
+  assert(lic14(points, parameters));
 }
 
-//the triangles are different
-//larger triangle shows up first
+// the triangles are different
+// larger triangle shows up first
 void test_true_instance2() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(1, -2), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(1, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0)};
-    Parameters parameters{.AREA1 = 12, .EPTS=2, .FPTS=5, .AREA2= 6 };
-    assert(lic14(points, parameters));
+  Points points{
+      Coordinate(1, -2), Coordinate(0, 0),  Coordinate(0, 0),  Coordinate(3, 4),
+      Coordinate(0, 0),  Coordinate(0, 0),  Coordinate(0, 0),  Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(5, -6), Coordinate(0, 0),  Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(1, -2), Coordinate(0, 0),  Coordinate(0, 0),
+      Coordinate(3, 4),  Coordinate(0, 0),  Coordinate(0, 0),  Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0),  Coordinate(1, -6), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0)};
+  Parameters parameters{.AREA1 = 12, .EPTS = 2, .FPTS = 5, .AREA2 = 6};
+  assert(lic14(points, parameters));
 }
 
-//there is only one triangle meeting both requirements
+// there is only one triangle meeting both requirements
 void test_true_instance3() {
-    Points points{Coordinate(1, -2), Coordinate(0, 0),
-                     Coordinate(3, 4), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),
-                     Coordinate(5, -6), Coordinate(0, 0), Coordinate(0, 0), Coordinate(0, 0),};
-    Parameters parameters{.AREA1 = 12, .EPTS=1, .FPTS=4, .AREA2= 50 };
-    assert(lic14(points, parameters));
+  Points points{
+      Coordinate(1, -2), Coordinate(0, 0), Coordinate(3, 4), Coordinate(0, 0),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0), Coordinate(5, -6),
+      Coordinate(0, 0),  Coordinate(0, 0), Coordinate(0, 0),
+  };
+  Parameters parameters{.AREA1 = 12, .EPTS = 1, .FPTS = 4, .AREA2 = 50};
+  assert(lic14(points, parameters));
 }
 
-//triangles are overlapping
+// triangles are overlapping
 void test_true_instance4() {
-    Points points{Coordinate(1, -2), Coordinate(10, 2), 
-                    Coordinate(2, 4), Coordinate(30, 4),
-                    Coordinate(5, -6), Coordinate(10, -60),};
-    Parameters parameters{.AREA1 = 20, .EPTS=1, .FPTS=1, .AREA2= 20 };
-    assert(lic14(points, parameters));
+  Points points{
+      Coordinate(1, -2), Coordinate(10, 2), Coordinate(2, 4),
+      Coordinate(30, 4), Coordinate(5, -6), Coordinate(10, -60),
+  };
+  Parameters parameters{.AREA1 = 20, .EPTS = 1, .FPTS = 1, .AREA2 = 20};
+  assert(lic14(points, parameters));
 }
-
-
 
 int main() {
-    test_true_instance1();
-    test_true_instance2();
-    test_true_instance3();
-    test_false_instance1();
-    test_false_instance2();
-    test_false_instance3();
-    test_false_instance4();
+  test_true_instance1();
+  test_true_instance2();
+  test_true_instance3();
+  test_false_instance1();
+  test_false_instance2();
+  test_false_instance3();
+  test_false_instance4();
 }
