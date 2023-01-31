d99684c852d0139248991ad585c2ed1aecedc62b
Pontus Söderlund <52171526+psoder@users.noreply.github.com>
2023-01-31 05:55:52 +0100

    fix: Add missing LIC declarations (#102)
    Fixes #101.
    
    - [x] Feature/fix PRs should add one atomic (as small as possible)
    feature or
      fix
    - [ ] If your PR adds a feature or fix it also needs to add or modify at
    least
      one test
    - [ ] All code in your PR needs to have been formatted by `clang-format`
    - [x] The merge commit title should follow our commit prefix convention
    of
      either "feat", "fix", "doc", or "refactor"
    - [x] The merge commit body should reference at least one issue

--- decide/lics.hpp
+++ decide/lics.hpp
@@ -6,6 +6,8 @@
 // Functions
 bool lic0(Points points, Parameters parameters);
 bool lic1(Points points, Parameters parameters);
+bool lic2(Points points, Parameters parameters);
+bool lic3(Points points, Parameters parameters);
 bool lic4(Points points, Parameters parameters);
 bool lic5(Points points, Parameters parameters);
 bool lic6(Points points, Parameters parameters);
@@ -16,5 +18,6 @@ bool lic10(Points points, Parameters parameters);
 bool lic11(Points points, Parameters parameters);
 bool lic12(Points points, Parameters parameters);
 bool lic13(Points points, Parameters parameters);
+bool lic14(Points points, Parameters parameters);
 
 #endif
