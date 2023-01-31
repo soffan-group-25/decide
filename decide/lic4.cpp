2ad5b909e6775d5bed4add945c16b920a3ae079c
Didrik Munther <dmu0817@gmail.com>
2023-01-31 02:42:44 +0100

    Format lic4

--- decide/lic4.cpp
+++ decide/lic4.cpp
@@ -1,86 +1,81 @@
 #include <decide/decide.hpp>
 
-/*There exists at least one set of Q_PTS (consecutive data points) that lie in more than QUADS
-(quadrants). Where there is ambiguity as to which quadrant contains a given point, priority
-of decision will be by quadrant number, i.e., I, II, III, IV. For example, the data point (0,0)
-is in quadrant I, the point (-l,0) is in quadrant II, the point (0,-l) is in quadrant III, the point
-(0,1) is in quadrant I and the point (1,0) is in quadrant I.*/
+/*There exists at least one set of Q_PTS (consecutive data points) that lie in
+more than QUADS (quadrants). Where there is ambiguity as to which quadrant
+contains a given point, priority of decision will be by quadrant number, i.e.,
+I, II, III, IV. For example, the data point (0,0) is in quadrant I, the point
+(-l,0) is in quadrant II, the point (0,-l) is in quadrant III, the point (0,1)
+is in quadrant I and the point (1,0) is in quadrant I.*/
 
-int quad(Coordinate point){
-    if (point.y>=0){
-        if (point.x>=0){
-            return 1;
-        }
-        else{
-            return 2;
-        }
+int quad(Coordinate point) {
+  if (point.y >= 0) {
+    if (point.x >= 0) {
+      return 1;
+    } else {
+      return 2;
     }
-    else{
-        if (point.x<=0){
-            return 3;
-        }
-        else{
-            return 4;
-        }
+  } else {
+    if (point.x <= 0) {
+      return 3;
+    } else {
+      return 4;
     }
+  }
 }
 
-bool unique(int arr[], int goal, int size){
-    int first=0;
-    int second=0;
-    int third=0;
-    int forth=0;
-    for (int i=0; i < size;i++){
-        switch (arr[i])
-        {
-        case 1:
-            if (first==0){
-                 first++;
-            }
-            break;
-        case 2:
-            if (second==0){
-                 second++;
-            }
-            break;
-        case 3:
-            if (third==0){
-                 third++;
-            }
-            break;
-        case 4:
-            if (forth==0){
-                 forth++;
-            }
-            break;
-        }
+bool unique(int arr[], int goal, int size) {
+  int first = 0;
+  int second = 0;
+  int third = 0;
+  int forth = 0;
+  for (int i = 0; i < size; i++) {
+    switch (arr[i]) {
+    case 1:
+      if (first == 0) {
+        first++;
+      }
+      break;
+    case 2:
+      if (second == 0) {
+        second++;
+      }
+      break;
+    case 3:
+      if (third == 0) {
+        third++;
+      }
+      break;
+    case 4:
+      if (forth == 0) {
+        forth++;
+      }
+      break;
     }
-    if ((first+second+third+forth)>goal){
+  }
+  if ((first + second + third + forth) > goal) {
+    return true;
+  }
+  return false;
+}
+
+bool lic4(Points points, Parameters param) {
+  int qpts = param.QPTS;
+  int npts = points.size();
+  if (qpts < 2 || qpts > npts) {
+    return false;
+  }
+  if (1 <= param.QUADS <= 3) {
+    for (int i = 0; i + qpts - 1 < npts; i++) {
+      int position[qpts];
+      for (int a = 0; a < qpts; a++) {
+        position[a] = quad(points[i + a]);
+      }
+      if (unique(position, param.QUADS, qpts)) {
         return true;
+      }
     }
     return false;
-}
-
-bool lic4(Points points, Parameters param){ 
-    int qpts=param.QPTS;
-    int npts=points.size();
-    if (qpts<2 || qpts>npts){
-        return false;
-    }
-    if (1<=param.QUADS<=3){
-        for (int i=0; i+qpts-1<npts;i++){
-            int position[qpts];
-            for (int a=0; a<qpts; a++){
-                position[a]=quad(points[i+a]);
-            }
-            if (unique(position, param.QUADS, qpts)){
-                return true;
-            }
-        }
-        return false;
-    }
-    else{
-        return false;
-    }
-
+  } else {
+    return false;
+  }
 }
--- decide/lic4.test.cpp
+++ decide/lic4.test.cpp
@@ -3,66 +3,71 @@
 #include <decide/decide.hpp>
 #include <decide/lics.hpp>
 
-
-
-//test that when QUADS = 1 then it will be true
-void test_true_instance1(){
-    Points points{Coordinate(1, 1), Coordinate(0, -1), Coordinate(0, 2),Coordinate(-5, -4), Coordinate(1, -9), Coordinate(0, 0)};
-    Parameters param{.QPTS=3, .QUADS=1};
-    assert(lic4(points,param));
+// test that when QUADS = 1 then it will be true
+void test_true_instance1() {
+  Points points{Coordinate(1, 1),   Coordinate(0, -1), Coordinate(0, 2),
+                Coordinate(-5, -4), Coordinate(1, -9), Coordinate(0, 0)};
+  Parameters param{.QPTS = 3, .QUADS = 1};
+  assert(lic4(points, param));
 }
-//the last four are different 
-void test_true_instance2(){
-    Points points{Coordinate(1, 1), Coordinate(0, 7), Coordinate(16, 9),Coordinate(-8, 4), Coordinate(1, -9), Coordinate(-7, -19)};
-    Parameters param{.QPTS=4, .QUADS=2};
-    assert(lic4(points,param));
-}
-
-//checking that it works if it is only looking for 3 quads
-void test_true_instance3(){
-    Points points{Coordinate(1, 1), Coordinate(0, -1), Coordinate(0, 2),Coordinate(-5, -4), Coordinate(1, -9), Coordinate(-1, 0)};
-    Parameters param{.QPTS=6, .QUADS=3};
-    assert(lic4(points,param));
+// the last four are different
+void test_true_instance2() {
+  Points points{Coordinate(1, 1),  Coordinate(0, 7),  Coordinate(16, 9),
+                Coordinate(-8, 4), Coordinate(1, -9), Coordinate(-7, -19)};
+  Parameters param{.QPTS = 4, .QUADS = 2};
+  assert(lic4(points, param));
 }
 
-//should be false
-//all points are the same so they can't be in different quads
-void test_false_instance1(){
-    Points points{Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1),Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1)};
-    Parameters param{.QPTS=3, .QUADS=2};
-    assert(!lic4(points,param));
+// checking that it works if it is only looking for 3 quads
+void test_true_instance3() {
+  Points points{Coordinate(1, 1),   Coordinate(0, -1), Coordinate(0, 2),
+                Coordinate(-5, -4), Coordinate(1, -9), Coordinate(-1, 0)};
+  Parameters param{.QPTS = 6, .QUADS = 3};
+  assert(lic4(points, param));
 }
 
-//should be false
-//all points are the same so they can't be in different quads
-//same as above but with 3 quads
-void test_false_instance2(){
-    Points points{Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1),Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1)};
-    Parameters param{.QPTS=3, .QUADS=3};
-    assert(!lic4(points,param));
+// should be false
+// all points are the same so they can't be in different quads
+void test_false_instance1() {
+  Points points{Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1),
+                Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1)};
+  Parameters param{.QPTS = 3, .QUADS = 2};
+  assert(!lic4(points, param));
 }
 
-//tests case where all are in the same quad so should be false
-void test_false_instance3(){
-    Points points{Coordinate(1, -1), Coordinate(1, -2), Coordinate(1, -14),Coordinate(8, -9), Coordinate(9, -7), Coordinate(8, -2)};
-    Parameters param{.QPTS=6, .QUADS=3};
-    assert(!lic4(points,param));
+// should be false
+// all points are the same so they can't be in different quads
+// same as above but with 3 quads
+void test_false_instance2() {
+  Points points{Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1),
+                Coordinate(1, 1), Coordinate(1, 1), Coordinate(1, 1)};
+  Parameters param{.QPTS = 3, .QUADS = 3};
+  assert(!lic4(points, param));
 }
 
+// tests case where all are in the same quad so should be false
+void test_false_instance3() {
+  Points points{Coordinate(1, -1), Coordinate(1, -2), Coordinate(1, -14),
+                Coordinate(8, -9), Coordinate(9, -7), Coordinate(8, -2)};
+  Parameters param{.QPTS = 6, .QUADS = 3};
+  assert(!lic4(points, param));
+}
 
-//tests cases where there are enough in 3 quads however they are not consecutive
-void test_false_instance4(){
-    Points points{Coordinate(1, 1), Coordinate(10, 4), Coordinate(-1, -1),Coordinate(-7, -9), Coordinate(-2, 5), Coordinate(-1, 2)};
-    Parameters param{.QPTS=3, .QUADS=3};
-    assert (!lic4(points,param));
+// tests cases where there are enough in 3 quads however they are not
+// consecutive
+void test_false_instance4() {
+  Points points{Coordinate(1, 1),   Coordinate(10, 4), Coordinate(-1, -1),
+                Coordinate(-7, -9), Coordinate(-2, 5), Coordinate(-1, 2)};
+  Parameters param{.QPTS = 3, .QUADS = 3};
+  assert(!lic4(points, param));
 }
 
 int main() {
-    test_true_instance1();
-    test_true_instance2();
-    test_true_instance3();
-    test_false_instance1();
-    test_false_instance2();
-    test_false_instance3();
-    test_false_instance4();
+  test_true_instance1();
+  test_true_instance2();
+  test_true_instance3();
+  test_false_instance1();
+  test_false_instance2();
+  test_false_instance3();
+  test_false_instance4();
 }
