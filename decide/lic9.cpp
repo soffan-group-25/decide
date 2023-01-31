58e30329462777c90a7c609024f3f195ceeb0593
Didrik Munther <dmu0817@gmail.com>
2023-01-31 02:58:19 +0100

    Fix formatting in lic9 function

--- decide/lic9.cpp
+++ decide/lic9.cpp
@@ -8,7 +8,6 @@ of three points is always the vertex of the angle. If either the first point or
 the last point (or both) coincide with the vertex, the angle is undefined and
 the LIC is not satisfied by those three points. When NUMPOINTS < 5, the
 condition is not met. 1 ≤ C PTS, 1 ≤ D PTS C PTS+D PTS ≤ NUMPOINTS−3*/
-
 double angle(Coordinate a, Coordinate b, Coordinate c) {
   double ax = a.x - b.x;
   double ay = a.y - b.y;
@@ -16,26 +15,27 @@ double angle(Coordinate a, Coordinate b, Coordinate c) {
   double by = a.y - c.y;
   double top = (ax * bx) + (ay * by);
   double bottom = sqrt(pow(ax, 2) + pow(ay, 2)) * sqrt(pow(bx, 2) + pow(by, 2));
+
   return acos(top / bottom);
 }
+
 bool lic9(Points points, Parameters parameters) {
   int c = parameters.CPTS + 1;
   int d = parameters.DPTS + 1;
   int numpoints = points.size();
   double epsilon = parameters.EPSILON;
 
-  if (numpoints < 5 || c - 1 < 1 || d - 1 < 1 || (c + d) > (numpoints - 1)) {
+  if (numpoints < 5 || c - 1 < 1 || d - 1 < 1 || (c + d) > (numpoints - 1))
     return false;
-  }
-  for (int i = 0; i + c + d < numpoints; i++) {
 
-    if (points[i] == points[i + c] || points[i + c] == points[i + c + d]) {
+  for (int i = 0; i + c + d < numpoints; i++) {
+    if (points[i] == points[i + c] || points[i + c] == points[i + c + d])
       continue;
-    }
+
     double ang = angle(points[i + c], points[i], points[i + c + d]);
-    if (ang < (pi - parameters.EPSILON) || ang > (pi + parameters.EPSILON)) {
+
+    if (ang < (pi - parameters.EPSILON) || ang > (pi + parameters.EPSILON))
       return true;
-    }
   }
 
   return false;
