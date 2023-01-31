ae95ce412fd309fe2cb3c61d27619bd7eda219dd
Pontus <52171526+psoder@users.noreply.github.com>
2023-01-31 06:25:10 +0100

    Use angle function definied in utils instead

--- decide/lic2.cpp
+++ decide/lic2.cpp
@@ -1,4 +1,5 @@
 #include <decide/decide.hpp>
+#include <decide/utils.hpp>
 
 /*There exists at least one set of three consecutive data points which form an
 angle such that: angle < (PI−EPSILON) or angle > (PI+EPSILON) The second of the
@@ -6,17 +7,6 @@ three consecutive points is always the vertex of the angle. If either the first
 point or the last point (or both) coincides with the vertex, the angle is
 undefined and the LIC is not satisfied by those three points. (0 ≤ EPSILON <
 PI)*/
-
-double angle(Coordinate a, Coordinate b, Coordinate c) {
-  double ax = a.x - b.x;
-  double ay = a.y - b.y;
-  double bx = a.x - c.x;
-  double by = a.y - c.y;
-  double top = (ax * bx) + (ay * by);
-  double bottom = sqrt(pow(ax, 2) + pow(ay, 2)) * sqrt(pow(bx, 2) + pow(by, 2));
-  return acos(top / bottom);
-}
-
 bool lic2(Points points, Parameters parameters) {
   if (points.size() < 3 || parameters.EPSILON < 0 || parameters.EPSILON >= pi) {
     return false;
@@ -26,7 +16,7 @@ bool lic2(Points points, Parameters parameters) {
     if (points[i + 1] == points[i] || points[i + 1] == points[i + 2]) {
       continue;
     }
-    double ang = angle(points[i + 1], points[i], points[i + 2]);
+    double ang = angle_between_points(points[i + 1], points[i], points[i + 2]);
     if (ang < (pi - parameters.EPSILON) || ang > (pi + parameters.EPSILON)) {
       return true;
     }
--- decide/lic9.cpp
+++ decide/lic9.cpp
@@ -1,5 +1,5 @@
 #include <decide/decide.hpp>
-#include <math.h>
+#include <decide/utils.hpp>
 
 /*There exists at least one set of three data points separated by exactly C PTS
 and D PTS consecutive intervening points, respectively, that form an angle such
@@ -8,17 +8,6 @@ of three points is always the vertex of the angle. If either the first point or
 the last point (or both) coincide with the vertex, the angle is undefined and
 the LIC is not satisfied by those three points. When NUMPOINTS < 5, the
 condition is not met. 1 ≤ C PTS, 1 ≤ D PTS C PTS+D PTS ≤ NUMPOINTS−3*/
-double angle(Coordinate a, Coordinate b, Coordinate c) {
-  double ax = a.x - b.x;
-  double ay = a.y - b.y;
-  double bx = a.x - c.x;
-  double by = a.y - c.y;
-  double top = (ax * bx) + (ay * by);
-  double bottom = sqrt(pow(ax, 2) + pow(ay, 2)) * sqrt(pow(bx, 2) + pow(by, 2));
-
-  return acos(top / bottom);
-}
-
 bool lic9(Points points, Parameters parameters) {
   int c = parameters.CPTS + 1;
   int d = parameters.DPTS + 1;
@@ -32,7 +21,8 @@ bool lic9(Points points, Parameters parameters) {
     if (points[i] == points[i + c] || points[i + c] == points[i + c + d])
       continue;
 
-    double ang = angle(points[i + c], points[i], points[i + c + d]);
+    double ang =
+        angle_between_points(points[i + c], points[i], points[i + c + d]);
 
     if (ang < (pi - parameters.EPSILON) || ang > (pi + parameters.EPSILON))
       return true;
