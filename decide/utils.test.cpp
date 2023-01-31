76008f6cf71c196da5951d2d9a7a616115233629
Pontus Söderlund <52171526+psoder@users.noreply.github.com>
2023-01-31 01:50:40 +0100

    feat: Add angle between points function (#96)
    Fixes #56.
    
    - [x] Feature/fix PRs should add one atomic (as small as possible)
    feature or
      fix
    - [x] If your PR adds a feature or fix it also needs to add or modify at
    least
      one test
    - [ ] All code in your PR needs to have been formatted by `clang-format`
    - [x] The merge commit title should follow our commit prefix convention
    of
      either "feat", "fix", "doc", or "refactor"
    - [x] The merge commit body should reference at least one issue

--- decide/utils.test.cpp
+++ decide/utils.test.cpp
@@ -2,9 +2,18 @@
 #include <cstdlib>
 #include <decide/utils.hpp>
 
+void test_angle_90_degree() {
+  double angle = angle_between_points(Coordinate(0, 1), Coordinate(0, 0),
+                                      Coordinate(1, 0));
+  assert((angle - pi / 4.0) < 1e-10);
+}
+
 void test_triangle_area() {
   double a = triangleArea(Coordinate(1, 2), Coordinate(-1, 1), Coordinate(0, 5));
   assert(a == 3.5);
 }
 
-int main() { test_triangle_area(); }
+int main() { 
+  test_triangle_area();
+  test_angle_90_degree();
+}
