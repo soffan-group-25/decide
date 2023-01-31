08703ce597f39874c9bf14586cd64323d190f36f
haval0 <56519858+haval0@users.noreply.github.com>
2023-01-31 10:43:42 +0100

    Add tests

--- decide/decide.test.cpp
+++ decide/decide.test.cpp
@@ -17,8 +17,36 @@ private:
   std::streambuf *old;
 };
 
+CMV calculateCMV(Points points, Parameters parameters);
 PUM calculatePUM(CMV cmv, LCM lcm);
 FUV calculateFUV(PUM &pum, PUV &puv);
+
+void test_cmv() {
+  Points points{Coordinate{0, 0},     Coordinate{2, 2},  Coordinate{3, 2},
+                Coordinate{100, 100}, Coordinate{4, 4},  Coordinate{3245, 232},
+                Coordinate{529, 234}, Coordinate{50, 1}, Coordinate{12, 12}};
+  Parameters parameters{.LENGTH1 = 5, .RADIUS1 = 49, .EPSILON = 3, .AREA1 = 50,
+      .QPTS = 2, .QUADS = 2, .DIST = 6, .NPTS = 3, .KPTS = 1, .APTS = 2, .BPTS = 3,
+      .CPTS = 1, .DPTS = 1, .EPTS = 1, .FPTS = 1, .GPTS = 2, .LENGTH2 = 8,
+      .RADIUS2 = 7.5, .AREA2 = 20};
+  CMV cmv = calculateCMV(points, parameters);
+
+  assert(cmv[0] = lic0(points, parameters));
+  assert(cmv[1] = lic1(points, parameters));
+  assert(cmv[2] = lic2(points, parameters));
+  assert(cmv[3] = lic3(points, parameters));
+  assert(cmv[4] = lic4(points, parameters));
+  assert(cmv[5] = lic5(points, parameters));
+  assert(cmv[6] = lic6(points, parameters));
+  assert(cmv[7] = lic7(points, parameters));
+  assert(cmv[8] = lic8(points, parameters));
+  assert(cmv[9] = lic9(points, parameters));
+  assert(cmv[10] = lic10(points, parameters));
+  assert(cmv[11] = lic11(points, parameters));
+  assert(cmv[12] = lic12(points, parameters));
+  assert(cmv[13] = lic13(points, parameters));
+  assert(cmv[14] = lic14(points, parameters));
+}
 
 void assert_pum_equal(PUM &p1, PUM &p2) {
   int n = p1.size();
