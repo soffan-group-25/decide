034bb6ba26eeebebfadb32dc3fea201352812bfc
Daniel Williams <dwilli@kth.se>
2023-01-31 01:02:47 +0100

    Fix LIC8 implementation

--- decide/lic8.cpp
+++ decide/lic8.cpp
@@ -1,22 +1,26 @@
 #include <decide/decide.hpp>
+#include <decide/lics.hpp>
+#include <decide/utils.hpp>
 
 /// There exists at least one set of three data points separated by exactly
-/// A_PTS and B_PTS consecutive intervening points, respectively, that
+/// APTS and BPTS consecutive intervening points, respectively, that
 /// cannot be contained within or on a circle of radius RADIUS1. The
 /// condition is not met when NUMPOINTS < 5.
-/// Clarification: we have our point, then A_PTS points, then our next point,
-/// then B_PTS, then our last point.
+/// Clarification: we have our point, then APTS points, then our next point,
+/// then BPTS, then our last point.
 ///
-/// 1 ≤ A_PTS, 1 ≤ B_PTS
-/// A_PTS + B_PTS ≤ (NUMPOINTS − 3)
+/// 1 ≤ APTS, 1 ≤ BPTS
+/// APTS + BPTS ≤ (NUMPOINTS − 3)
 bool lic8(Points points, Parameters parameters) {
-    int apts = parameters.A_PTS;
-    int bpts = parameters.B_PTS;
+  int apts = parameters.APTS;
+  int bpts = parameters.BPTS;
 
-    for (int i = 0; i < points.size() - (apts+bpts+2); ++i){
-        if (contained_in_circle(points[i], points[i+apts+1], points [i+apts+1+bpts+1], parameters.RADIUS1)){
-            return true;
-        }
+  for (int i = 0; i < points.size() - (apts + bpts + 2); ++i) {
+    if (contained_in_circle(points[i], points[i + apts + 1],
+                            points[i + apts + 1 + bpts + 1],
+                            parameters.RADIUS1)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
