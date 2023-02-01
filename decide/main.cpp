1da9dafc378ec324143b707f95fa02df52908b58
Daniel Williams <dwilli@kth.se>
2023-02-01 15:12:55 +0100

    Add default values

--- decide/main.cpp
+++ decide/main.cpp
@@ -1,13 +1,15 @@
 #include <decide/decide.hpp>
 
 int main() {
-  LCM lcm;
-  PUV puv;
+  LCM lcm(15, std::vector<Connector>(15));
+  PUV puv(15);
 
-  Points points;
-  Parameters parameters;
+  Points points{Coordinate{67, 43}, Coordinate{4, 2}, Coordinate{0, 2},
+                Coordinate{2, 5}, Coordinate{23, 23}};
+  Parameters parameters{};
 
   CMV cmv = calculateCMV(points, parameters);
+
   PUM pum = calculatePUM(cmv, lcm);
   FUV fuv = calculateFUV(pum, puv);
 
